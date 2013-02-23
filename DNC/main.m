9342bff8bc5ffaaa7b5775ea858d900dd81a80df
Admin <Admin@Admins-Mac-10.local>
2013-02-23 10:31:04 +0200

    Initial Commit

---
+++ DNC/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  DNC
+//
+//  Created by Admin on 2/23/13.
+//  Copyright (c) 2013 Admin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
